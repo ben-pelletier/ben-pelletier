142fa9b97c8a932d88515dd9e5b1ce3fc584045c
Ben Pelletier <ben.epelletier@gmail.com>
2017-01-11 01:06:05 -0500

    now points to sass skeleton

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 require('../node_modules/font-awesome/css/font-awesome.css')
-require('./assets/libs/Skeleton-2.0.4/css/Skeleton.css')
-require('./assets/libs/Skeleton-2.0.4/css/normalize.css')
+require('./assets/libs/skeleton/skeleton.scss')
 require('./assets/css/main.styl');
 require('./assets/js/common.js');
