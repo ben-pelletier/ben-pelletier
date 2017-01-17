78c91bd08ce30df6dbd5eb5173baa99770df17b6
Ben Pelletier <ben.epelletier@gmail.com>
2017-01-17 12:57:13 -0500

    added includes for resume and coldfusion

--- src/index.js
+++ src/index.js
@@ -4,3 +4,6 @@ require('../node_modules/font-awesome/css/font-awesome.css')
 require('./assets/libs/skeleton/skeleton.scss')
 require('./assets/css/main.styl');
 require('./assets/js/common.js');
+
+require('file-loader?name=/assets/contact.cfc!./assets/classes/contact.cfc');
+require('file-loader?name=/ben_pelletier_resume.pdf!./assets/ben_pelletier_resume.pdf');
